300e36e0dd84f38d7e52b90fa3a25a7c8f3a7118
41U2 <alesha_u@mail.ru>
2021-12-19 18:32:20 +0300

    Попробовал создать страницу для пользователя с обращением к бд, получилось

---
+++ DriftCompetitionWeb/DriftCompetitionWeb/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+using DriftCompetitionWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DriftCompetitionWeb.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace DriftCompetitionWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly DriftCompetitionDevice driftCompetitionDevice;
+
+        public UserController(ApplicationDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+            driftCompetitionDevice = new DriftCompetitionDevice(dbContext);
+        }
+
+        public IdentityUser CurrentUser()
+        {
+            var userTmp = _userManager.GetUserAsync(HttpContext.User);
+            return userTmp.Result;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            IdentityUser currentUser = CurrentUser();
+            ViewBag.Cars = new List<Car> { };
+            IEnumerable<Car> usersCars = driftCompetitionDevice.carsRepository.UsersCars(currentUser);
+            if (currentUser != null)
+                ViewBag.Cars = usersCars.ToList();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
